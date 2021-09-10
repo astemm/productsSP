e2534391321c7b26e14d6531fa594186976e69fc
andriy1lan <andriy1co@meta.ua>
2021-09-10 23:37:37 +0300

    Add startpage, header and footer

--- ProductsUI/src/app/app-routing.module.ts
+++ ProductsUI/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from '../app/product-list/product-list.component';
 import { ViewProductComponent } from '../app/view-product/view-product.component';
+import { FirstPageComponent } from '../app/first-page/first-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: '', redirectTo: 'firstpage', pathMatch: 'full' },
+  { path: 'firstpage', component: FirstPageComponent},
   { path: 'products',
   children: [{path:':productId',component: ViewProductComponent},
   {path: '', component: ProductListComponent}]}
--- ProductsUI/src/app/app.module.ts
+++ ProductsUI/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ViewProductComponent } from './view-product/view-product.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FirstPageComponent } from './first-page/first-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ViewProductComponent
+    ViewProductComponent,
+    HeaderComponent,
+    FooterComponent,
+    FirstPageComponent
   ],
   imports: [
     BrowserModule,
